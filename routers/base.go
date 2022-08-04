19cb3a1e253d6aac2e33ab92ed7d2c60a2f2b700
wangdong <614545802@qq.com>
2022-08-04 21:32:48 +0800

    修改.1.crud修改成curd.2.修改flag不生效问题

--- routers/base.go
+++ routers/base.go
@@ -11,7 +11,7 @@ import (
 func init() {
 	_ = event.Subscribe(consts.ROUTER_INIT_EVENT, func(r *gin.Engine) {
 		ctl := controller.BaseController{}
-		v1 := r.Group("/curd")
+		v1 := r.Group("/crud")
 		v1.Use(middleware.CrudAuth())
 		{
 			v1.POST("/handler", ctl.Handler)
